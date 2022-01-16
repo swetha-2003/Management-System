30037b518bb1c999e2f5e7e651b5a8fe710a36d7
swetha-2003 <swethas.it20@bitsathy.ac.in>
2022-01-16 13:18:25 +0530

    initial commit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ Base = declarative_base()
 
 app = Flask("Student Activity Management")
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/flaskapp"
-app.config['SQLALCHEMY_DATABASE_URI'] ="postgres://frmdfmenlbxmpo:c483f8bc87501ce87501bf747ef2ba6996fa222b33e21a841d11aa7345529889@ec2-34-230-198-12.compute-1.amazonaws.com:5432/d4i53qvgs5pa56"
+app.config['SQLALCHEMY_DATABASE_URI'] =" postgres://ohqdbwcuttjdky:adb31557414ffda1df764dd507a7703b5e06249d99f88eabaf3dcd1d34b4e934@ec2-34-230-198-12.compute-1.amazonaws.com:5432/dch1chub6p3udn"
 app.secret_key = "studentactivitymanagement"
 UPLOAD_FOLDER = 'C:/Users/sweth/PycharmProjects/flaskProject1/static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -34,7 +34,8 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-engine = create_engine("mysql://root:@localhost/flaskapp",echo = False)
+# engine = create_engine("mysql://root:@localhost/flaskapp",echo = False)
+engine = create_engine("postgres://ohqdbwcuttjdky:adb31557414ffda1df764dd507a7703b5e06249d99f88eabaf3dcd1d34b4e934@ec2-34-230-198-12.compute-1.amazonaws.com:5432/dch1chub6p3udn",echo = False)
 engine.connect()
 con=engine.connect()
 
